c51b827ccc6c1e785732b78184e6b34d67423a5e
Alex Nurci <alexnurci@Alexs-MacBook-Air.local>
2025-06-20 15:41:40 -0400

    Added 3 tasks

--- main.js
+++ main.js
@@ -17,3 +17,34 @@ addColFeature.onclick = function () {
     row.appendChild(document.createElement("td"));
   }
 };
+
+//change color
+const root = document.getElementById("root");
+root.addEventListener("click", (event) => {
+  console.log(event.target.tagName);
+  console.log(event.target);
+  console.log(event.target.style.backgroundColor);
+  const color = document.getElementById("color-select");
+  if(event.target.tagName == "TD"){
+    const selected = color.value;
+    event.target.style.backgroundColor = selected;
+  }
+  const uncoloredButton = document.getElementById("fill-uncolored grid"); //fill uncolored grid
+  uncoloredButton.addEventListener("click", () => {
+    allTD = document.querySelectorAll('td');
+    allTD.forEach(td => {
+      if(td.style.backgroundColor == ""){
+        const selected = color.value;
+        td.style.backgroundColor = selected;
+      }
+    });
+  });
+  const fillButton = document.getElementById("fill-grid");//fill grid
+  fillButton.addEventListener("click", () => {
+    allTD = document.querySelectorAll('td');
+    allTD.forEach(td => {
+      const selected = color.value;
+      td.style.backgroundColor = selected;
+    });
+  });
+});
