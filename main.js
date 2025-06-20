e5f8003fb45e7b24bc869b66ac2790e483fac15e
Bryan Zhong <bryan.zhong46@stu.bmcc.cuny.edu>
2025-06-20 14:18:35 -0400

    Added button and event listener to remove the rightmost column from the grid

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ root.addEventListener("click", (event) => {
   console.log(event.target.tagName);
   console.log(event.target);
 });
+
+// Bryan - remove column from grid
+const removeColButton = document.getElementById("remove-column");
+removeColButton.addEventListener("click", (event) => {
+  
+  // Collect al the rows in the table into an array
+  const rows = Array.from(document.getElementsByTagName("tr"));
+
+  // Remove the last child of each row
+  for (const row of rows) {
+    const lastChild = row.lastElementChild;
+    if (lastChild) {
+      row.removeChild(lastChild);
+    }
+  }
+
+});
