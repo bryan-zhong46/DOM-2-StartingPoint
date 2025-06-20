9f9c6a0fab060a12ed4233ac6377996525d760e4
KidCharles <sircharlyrivera@gmail.com>
2025-06-20 15:39:07 -0400

    Implemented remove-row feature and added remove row button

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-// Add row feature
 const addRowFeature = document.getElementById("add-row");
 const addColFeature = document.getElementById("add-column")
+const removeRowFeature = document.getElementById("remove-row")
 const tbody = document.querySelector("table tbody");
 
+// Add row feature
 addRowFeature.onclick = function () {
   const cols = tbody.rows[0].cells.length;
   const tr = document.createElement("tr");
@@ -15,5 +16,13 @@ addRowFeature.onclick = function () {
 addColFeature.onclick = function () {
   for (let row of tbody.rows) {
     row.appendChild(document.createElement("td"));
+  }
+};
+
+
+// Remove-rows feature
+removeRowFeature.onclick = function () {
+  if (tbody.rows.length > 0) {
+    tbody.removeChild(tbody.rows[tbody.rows.length - 1]);
   }
 };
