08f876220ed6d1a8d521a0e38efb7ca1cebef43b
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-06-20 14:42:12 -0400

    Implemented mouseover coloring

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 // Please feel free to change the JS as you see fit! This is just a starting point.
 
 const root = document.getElementById("root");
-root.addEventListener("click", (event) => {
-  console.log(event.target.tagName);
-  console.log(event.target);
-});
+let isMouseDown = false;
+  root.addEventListener("mousedown", (event) => {
+    if (event.target.tagName.toLowerCase() === "td") {
+      isMouseDown = true;
+      const color = document.getElementById("color-select");
+      const selected = color.value;
+      event.target.style.backgroundColor = selected;
+    }
+  });
+
+root.addEventListener("mouseover", (event) => {
+  if (isMouseDown && event.target.tagName.toLowerCase() === "td") {
+    const color = document.getElementById("color-select");
+    const selected = color.value;
+    event.target.style.backgroundColor = selected;
+  }
+  });
+
+  root.addEventListener("mouseup", () => {
+    isMouseDown = false;
+  });
